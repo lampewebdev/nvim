91d0b4e7cd0118cbd48a690952a4f9454b5d3ed2
Michael Lazarski <lampe@Michaels-MacBook-Pro.local>
2023-03-10 22:25:23 +0100

    update

--- lua/lampe/packer.lua
+++ lua/lampe/packer.lua
@@ -4,18 +4,18 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  	use({ 'rose-pine/neovim', as = 'rose-pine' })
-	vim.cmd('colorscheme rose-pine')
-   use {
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    vim.cmd('colorscheme rose-pine')
+    use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
@@ -24,32 +24,45 @@ return require('packer').startup(function(use)
     }
 
     use('nvim-treesitter/playground')
+    use('ThePrimeagen/vim-be-good')
     use('ThePrimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
 
     use {
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v1.x',
-	    requires = {
-		    -- LSP Support
-		    {'neovim/nvim-lspconfig'},             -- Required
-		    {'williamboman/mason.nvim'},           -- Optional
-		    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		    -- Autocompletion
-		    {'hrsh7th/nvim-cmp'},         -- Required
-		    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		    {'hrsh7th/cmp-buffer'},       -- Optional
-		    {'hrsh7th/cmp-path'},         -- Optional
-		    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-		    -- Snippets
-		    {'L3MON4D3/LuaSnip'},             -- Required
-		    {'rafamadriz/friendly-snippets'}, -- Optional
-	    }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
     }
     use('liuchengxu/vim-which-key')
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 100
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
 
